afa5d50bfd2ba23ca3fd8ab08f67e15d1aeb06b1
Khuong Do <khuongdo@nvg.vn>
2021-03-26 16:36:34 +0700

    feat: implement the app using TDD

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,132 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App, { AddToDo, ToDoItem, ToDoList } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("AddToDo", () => {
+  const defaultProps = {
+    addToDo: jest.fn(),
+  };
+
+  it("should render successfully with the default props", () => {
+    const { container } = render(<AddToDo {...defaultProps} />);
+    expect(container).toBeDefined();
+  });
+
+  it("should render an input field", () => {
+    render(<AddToDo {...defaultProps} />);
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it('should display "Add an item" as a placehodler', () => {
+    render(<AddToDo {...defaultProps} />);
+    expect(screen.getByPlaceholderText("Add an item")).toBeInTheDocument();
+  });
+
+  it("should allow typing", () => {
+    render(<AddToDo {...defaultProps} />);
+    const input = screen.getByRole("textbox") as HTMLInputElement;
+    const value = "test";
+    fireEvent.change(input, { target: { value } });
+    expect(input.value).toBe(value);
+  });
+
+  it("should call clear the input value and call addToDo on hiting the Enter key", () => {
+    render(<AddToDo {...defaultProps} />);
+    const input = screen.getByRole("textbox") as HTMLInputElement;
+    const value = "test";
+    fireEvent.change(input, { target: { value } });
+    fireEvent.keyDown(input, { code: "Enter" });
+    expect(input.value).toBe("");
+    expect(defaultProps.addToDo).toHaveBeenCalledWith(value);
+  });
+
+  it("should not call addToDo when hitting another key other than Enter", () => {
+    render(<AddToDo {...defaultProps} />);
+    const input = screen.getByRole("textbox") as HTMLInputElement;
+    const value = "test";
+    fireEvent.change(input, { target: { value } });
+    fireEvent.keyDown(input, { code: "Alt" });
+    expect(defaultProps.addToDo).not.toHaveBeenCalled();
+  });
+});
+
+describe("ToDoItem", () => {
+  const defaultProps = { toDo: "Do something", removeToDo: jest.fn() };
+  it("should render successfully with the default props", () => {
+    const { container } = render(<ToDoItem {...defaultProps} />);
+    expect(container).toBeDefined();
+  });
+
+  it('should display a text passed to it as the "toDo" prop', () => {
+    render(<ToDoItem {...defaultProps} />);
+    expect(screen.getByText(defaultProps.toDo)).toBeInTheDocument();
+  });
+
+  it('should display a close button labeled "X"', () => {
+    render(<ToDoItem {...defaultProps} />);
+    const closeButton = screen.getByRole("button");
+    expect(closeButton).toHaveTextContent("X");
+  });
+
+  it("should call removeToDo when clicking on the Close button", () => {
+    render(<ToDoItem {...defaultProps} />);
+    const closeButton = screen.getByRole("button");
+    fireEvent.click(closeButton);
+    expect(defaultProps.removeToDo).toHaveBeenCalledWith(defaultProps.toDo);
+  });
+});
+
+describe("ToDoList", () => {
+  const defaultProps = {
+    toDos: ["action 1", "action 2"],
+    removeToDo: jest.fn(),
+  };
+  it("should render successfully with the default props", () => {
+    const { container } = render(<ToDoList {...defaultProps} />);
+    expect(container).toBeDefined();
+  });
+
+  it("should display items in the toDos props", () => {
+    render(<ToDoList {...defaultProps} />);
+    expect(screen.getByText(defaultProps.toDos[0])).toBeInTheDocument();
+    expect(screen.getByText(defaultProps.toDos[1])).toBeInTheDocument();
+  });
+});
+
+describe("App", () => {
+  it("should render successfully", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should display an input field", () => {
+    render(<App />);
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("should allow adding and removing todo items", async () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+
+    const firstAction = "Action 1";
+    fireEvent.change(input, { target: { value: firstAction } });
+    fireEvent.keyDown(input, { code: "Enter" });
+    expect(screen.queryByText(firstAction)).toBeDefined();
+
+    const secondAction = "Action 2";
+    fireEvent.change(input, { target: { value: secondAction } });
+    fireEvent.keyDown(input, { code: "Enter" });
+    expect(screen.queryByText(secondAction)).toBeDefined();
+
+    const closeButtons = screen.getAllByText("X");
+    expect(closeButtons).toHaveLength(2);
+
+    const firstCloseButton = closeButtons[0];
+    fireEvent.click(firstCloseButton);
+    expect(screen.queryByText(firstAction)).toBeNull();
+
+    const secondCloseButton = closeButtons[1];
+    fireEvent.click(secondCloseButton);
+    expect(screen.queryByText(secondAction)).toBeNull();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import "./App.css";
 
-function App() {
+interface AddToDoProps {
+  addToDo(value: string): void;
+}
+
+export const AddToDo = ({ addToDo }: AddToDoProps) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = useCallback((event) => {
+    setValue(event.target.value);
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.code === "Enter") {
+        setValue("");
+        addToDo(event.target.value);
+      }
+    },
+    [addToDo]
+  );
+
+  return (
+    <input
+      placeholder="Add an item"
+      onKeyDown={handleKeyDown}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+};
+
+interface ToDoItemProps {
+  toDo: string;
+  removeToDo(value: string): void;
+}
+
+export const ToDoItem = ({ toDo, removeToDo }: ToDoItemProps) => {
+  return (
+    <li>
+      <span>{toDo}</span>
+      <button onClick={() => removeToDo(toDo)}>X</button>
+    </li>
+  );
+};
+
+interface ToDoListProps {
+  toDos: string[];
+  removeToDo(value: string): void;
+}
+
+export const ToDoList = ({ toDos, removeToDo }: ToDoListProps) => {
+  return (
+    <ul>
+      {toDos.map((toDo) => (
+        <ToDoItem key={toDo} toDo={toDo} removeToDo={removeToDo} />
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [toDos, setToDos] = useState<string[]>([]);
+
+  const addToDo = useCallback((value: string) => {
+    setToDos((currentToDos) => [...currentToDos, value]);
+  }, []);
+
+  const removeToDo = useCallback((value: string) => {
+    setToDos((currentToDos) => currentToDos.filter((toDo) => toDo !== value));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <AddToDo addToDo={addToDo} />
+        <ToDoList toDos={toDos} removeToDo={removeToDo} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
